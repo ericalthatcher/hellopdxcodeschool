0ec3dda71e7ab95018afc095c037f1bc84c6c9d4
Erica Thatcher <ericalthatcher@gmail.com>
2013-04-24 01:02:46 -0700

    initial hello world commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hellocodeschool::Application.config.secret_token = 'c3c63d9187da34c24bc5ac51e322c6b8bb440e10fe06939ae0cf69c24bd8564fc1e5d41a45322bfe832e51232002d6c8779559bb88511a571320139c8d5daf24'
